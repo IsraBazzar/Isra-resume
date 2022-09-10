3c223c36272f0e307d7dcf23551e18099c41b351
Isra bazzar <isra2.badaha@gmail.com>
2022-09-10 03:33:51 +0300

    adding project files

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# Isra-resume
-Landing page project for UDACITY FrontEnd Nanodegree
+# Landing Page Project
+
+## Table of Contents
+
+* [Project Summary](#Project-summary)
+* [Development Strategy](#Development-Strategy)
+* [Resources](#resources)
+
+
+## Project Summary
+[(Back to top)](#table-of-contents)
+
+This project is required to complete the requirements Frontend Nanodegree by Udacity, it aims to give you real-world scenarios of manipulating the DOM. The functionality you will be using serves two purposes: to prepare you for appending dynamically added data to the DOM, and to show you how javascript can improve the usability of an otherwise static site. This project barely touches the surface of what is possible, but it does use some incredibly common events, methods, and logic.
+
+## Development Strategy
+[(Back to top)](#table-of-contents)
+
+- I started by cloning the starter code provided by Udacity.
+- Then I added my design (colors, font style, and some other) from my last project, since I preferred to unify my works
+- Added a function to create navigation list dynamically.
+- Added functionality to distinguish the section in view.
+- Added the functionality to scroll to sections.
+
+## Resources
+[(Back to top)](#table-of-contents)
+-Starter code from Udacity: https://github.com/udacity/fend/tree/refresh-2019/projects/landing-page
+- Color palette from https://coolors.co
+- Fonts from: https://fonts.google.com/
+- ipsum text from: https://loremipsum.io/generator/?n=1&t=p
+- favicon AND social media icons from: https://icons8.com/icons
+
---
+++ js/app.js
@@ -0,0 +1,119 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+
+const mainSections = Array.from(document.querySelectorAll("section"));
+let  navList = document.getElementById("navbar__list");
+
+let navLink;
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+
+/**
+ * createListItem function: generate items for navigation listdepending on sections
+ */
+function createListItem(){
+    /**
+     * 
+     */
+    for (let i = 0; i < mainSections.length; i++) {
+        let navItem = document.createElement("li");
+        navLink = document.createElement("a");
+        //create link navigation item to append for list item
+        navLink.innerHTML = mainSections[i].getAttribute("data-nav");
+        navLink.classList.add("menu__link");
+        //define attributes for navigation link
+        navLink.setAttribute("href", "#section" + (i + 1));
+        navLink.setAttribute("data-link", mainSections[i].getAttribute("data-nav"));
+        
+        navItem.append(navLink);
+        //append list item to navigation list
+        navList.append(navItem);
+      }
+}
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+/** Build menu 
+ * Call our function to build navigation menu
+*/ 
+createListItem();
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+/**
+ * Added functionality to distinguish the section in view
+ * EventListener to scrolling (while scrolling the section in action will be reset)
+ */
+document.addEventListener("scroll", function isSectionInViewport() {
+    mainSections.forEach((section) => {
+      if (window.scrollY >= section.offsetHeight) {
+        section.classList.add("active__section");
+      } else {
+        section.classList.remove("active__section");
+      }
+    });
+  });
+ 
+  /**  
+   * Build list of links for navigation
+   * */
+  const linksList = document.querySelectorAll("a[href^='#section']");
+  
+  linksList.forEach(function (link) 
+  {
+    link.addEventListener("click", () => {
+      linksList.forEach((link) => {
+        if (link.classList.contains("active__link")) {
+          link.classList.remove("active__link");
+        }
+      });
+  
+      link.classList.add("active__link");
+  
+      let ref = link.href.split("#section");
+      ref = "#section" + ref[1];
+      /**
+       * Scroll to reference for each link
+       */
+      window.scroll({
+        behavior: "smooth",
+        left: 0,
+        top: document.querySelector(ref).offsetTop,
+      });
+    });
+  });
